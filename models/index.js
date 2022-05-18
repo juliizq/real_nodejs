9f677751e293d6a3292c2cc1be9aad897951f283
Juliana Izquierdo <86469003+juliizq@users.noreply.github.com>
2022-05-18 11:39:57 +0200

    env

--- models/index.js
+++ models/index.js
@@ -6,6 +6,7 @@ const reviewModel = require('./Review')
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,  { 
   host: process.env.DB_HOST,
+ port : process.env.DB_PORT,
   dialect: "mysql",
   logging: false
 })
