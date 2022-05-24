dc85c354e6169bcc8045b4bc509c73ceb1c766ae
Juliana Izquierdo <86469003+juliizq@users.noreply.github.com>
2022-05-24 09:17:39 +0200

    config

--- config/db.config.js
+++ config/db.config.js
@@ -1,18 +1,8 @@
 const Sequelize = require('sequelize');
-
 const db = {};
  
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// Recuperar los modelos 
-// Ejemplos >
-db.user = require('../model/user.model.js')(sequelize, Sequelize);
-// db.role = require('../model/role.model.js')(sequelize, Sequelize);
-
-//Relaciones entre modelos
- 
-// db.role.belongsToMany(db.user, { through: 'user_roles', foreignKey: 'roleId', otherKey: 'userId'});
-// db.user.belongsToMany(db.role, { through: 'user_roles', foreignKey: 'userId', otherKey: 'roleId'});
 
 module.exports = db;
---
+++ config/roles_list.js
@@ -0,0 +1,7 @@
+const ROLES_LIST = {
+    "Admin" : "ADMIN",
+    "Moderator" : "MODERATOR",
+    "User" : "USER"
+}
+
+module.exports = ROLES_LIST
