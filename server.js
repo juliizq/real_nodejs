dd3a32674d56a858762356f8b3f1b3cd04fe593e
Juliana Izquierdo <86469003+juliizq@users.noreply.github.com>
2022-05-30 17:12:48 +0200

    Add imported routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
 const cookieParser = require('cookie-parser');
+const verifyJWT = require('./middleware/verifyJWT');
 
 require("dotenv").config();
 
@@ -10,25 +11,35 @@ require("dotenv").config();
 //Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
+// Middleware 
+app.use(express.json());
+
+
 // Import the routes 
 const userRoute = require('./routes/user.routes');
 const reviewRoute = require('./routes/review.routes')
 const authRoute = require('./routes/auth.routes');
-const refreshTokenRoute = require('./routes/refresh.routes')
+const refreshTokenRoute = require('./routes/refresh.routes');
+const logoutRoute = require('./routes/logout.routes');
+const registerRoute = require('./routes/register.routes')
 
-// Middleware 
-app.use(express.json());
+
 
 //Middleware for cookies
 app.use(cookieParser());
 
 // Routes middleware 
-app.use('/api/users', userRoute);
-app.use('/api/reviews', reviewRoute);
+app.use('/register', registerRoute)
 app.use('/api/auth', authRoute);
-app.use('/refresh', refreshTokenRoute)
+app.use('/refresh', refreshTokenRoute);
+app.use('/logout', logoutRoute);
+app.use('/api/reviews', reviewRoute);
 
-require("./models");
+app.use(verifyJWT);
+app.use('/api/users', userRoute);
+
+
+require('./models');
 
 
 app.listen(3000, () => console.log('Server up and running!'))
