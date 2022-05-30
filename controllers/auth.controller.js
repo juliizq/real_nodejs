eb57744cde1b0e944c5dbd4b9faf01a3d77f6c7c
Juliana Izquierdo <86469003+juliizq@users.noreply.github.com>
2022-05-30 17:08:59 +0200

    Add register controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,47 +1,13 @@
 const bcrypt = require('bcrypt');
+const { object } = require('joi');
 const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 const db = require('../models');
-const { registerValidation, loginValidation } = require('../validations/auth.validations');
-const saltRound = +process.env.SALT_ROUND || 10;
+const { loginValidation } = require('../validations/auth.validations');
+
 require('dotenv').config();
 
 module.exports = {
-    async register(req,res){
-
-        try {
-
-            const validation = registerValidation.validate(req.body);
-            if(validation.error){
-                return res.status(400).json(validation.error)
-            }
-
-            const exist = await db.User.findOne({ where : { email : req.body.email}})
-
-            if(exist){
-                return res.status(400).json({ error : 'Email already exist!'})
-            }
-            
-            const salt = bcrypt.genSaltSync(saltRound);
-            const hash = bcrypt.hashSync(req.body.password, salt);
-            const user = {
-                email : req.body.email,
-                password : hash,
-                firstName : req.body.firstName,
-                lastName : req.body.lastName,
-                role : req.body.role,
-                country : req.body.country,
-                city : req.body.city
-            };
-    
-            const savedUser =  await db.User.create(user);
-            res.json(savedUser);
-
-        }catch (err){
-            res.status(400).json(err)
-        }
-    },
-    
     async login(req,res){
 
         try {
@@ -68,9 +34,15 @@ module.exports = {
             }
 
             const accessToken = jwt.sign(
-                { userId : user.id, firstName: user.firstName }, 
+                {
+                    'UserInfo': {
+                        'userId': user.id,
+                        'firstName': user.firstName,
+                        'role': user.role
+                    }
+                },
                 process.env.ACCESS_TOKEN_SECRET,
-                {expiresIn : '10m'}
+                { expiresIn: '10m' }
             );
 
             const refreshToken = jwt.sign(
@@ -88,8 +60,6 @@ module.exports = {
 
         } catch (err) {
             res.status(400).json(err)
-        }
-               
+        }  
     }
-
 }
